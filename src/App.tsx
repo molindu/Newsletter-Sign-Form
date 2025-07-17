a68fd50e8d09eee0d7397cdfd03fb2827810ca79
molindu <molinduyasodha@gmail.com>
2025-07-17 09:00:12 +0530

    finished coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import './App.css'
+import DesktopIcon from './assets/icons/illustration-sign-up-desktop.svg'
+import MobileIcon from './assets/icons/illustration-sign-up-mobile.svg'
+import TabletIcon from './assets/icons/illustration-sign-up-tablet.svg'
+import IconList from './assets/icons/icon-list.svg'
+import Form from './components/From.tsx';
 
 function App() {
     return (
         <main>
-
+            <div className=" flex p-0 sm:p-4 bg-White rounded-[18px] m-4 flex-col-reverse sm:flex-row">
+                <div className="form-control">
+                    <h1>Stay updated!</h1>
+                    <div>Join 60,000+ product managers receiving monthly updates on:</div>
+                    <div className={'form-text flex-col'}>
+                        <div className={'form-text'}>
+                            <img src={IconList} alt="icon list"/>
+                            Product discovery and build what matters
+                        </div>
+                        <div className={'form-text'}>
+                            <img src={IconList} alt="icon list"/>
+                            Measuring to ensure updates are a success
+                        </div>
+                        <div className={'form-text'}>
+                            <img src={IconList} alt="icon list"/>
+                            And much more!
+                        </div>
+                    </div>
+                    <Form/>
+                </div>
+                <picture className={'picture'}>
+                    <source media="(max-width: 640px)" srcSet={MobileIcon}/>
+                    <source media="(max-width: 1024px)" srcSet={TabletIcon}/>
+                    <img
+                        src={DesktopIcon}
+                        alt={'Desktop Icon'}
+                        className={'item-card-image'}
+                    />
+                </picture>
+            </div>
         </main>
     )
 }
---
+++ src/components/From.tsx
@@ -0,0 +1,82 @@
+import * as yup from "yup"
+import {yupResolver} from "@hookform/resolvers/yup"
+import {useForm} from "react-hook-form"
+import {useState} from "react";
+import SuccessIcon from '../assets/icons/icon-success.svg'
+
+type FormData = {
+    email: string;
+};
+
+const From = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [submittedEmail, setSubmittedEmail] = useState("");
+
+    const validationSchema = yup.object({
+        email: yup
+            .string()
+            .required("This field is required")
+            .email("Valid email required"),
+    });
+
+    const {register, reset, handleSubmit, formState: {errors}} = useForm<FormData>({
+        resolver: yupResolver(validationSchema),
+        defaultValues: {
+            email: "",
+        }
+    })
+
+    const clearAll = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        reset()
+        setIsOpen(false);
+    }
+
+    const onSuccess = (data: FormData) => {
+        setSubmittedEmail(data.email);
+        setIsOpen(true);
+    };
+
+    const submission = handleSubmit(onSuccess)
+    return (
+
+        <form onSubmit={submission} className={'w-full flex flex-col gap-4'}>
+            <div className={'form-input '}>
+                <div className={'label'}>
+                    <label htmlFor="email">Email address</label>
+                    {errors.email && <span>{errors.email.message}</span>}
+                </div>
+                <div className={`input-container ${errors.email && 'input-error'}`}>
+                    <input
+                        type="email"
+                        placeholder={'email@company.com'}
+                        {...register("email")}
+                    />
+                </div>
+            </div>
+            <div>
+                <button className={'submit-btn'} type={'submit'}>
+                    Subscribe to monthly newsletter
+                </button>
+            </div>
+            {isOpen && (
+                <div className={'modal-container'}>
+                    <div className={'modal-content'}>
+                        <img src={SuccessIcon} alt="Success Icon" className={'w-12'}/>
+                        <h1>Thanks for subscribing!</h1>
+                        <div>
+                            A confirmation email has been sent to <strong>{submittedEmail}</strong> . Please open it and click the button inside to
+                            confirm your subscription.
+                        </div>
+                        <button className={'submit-btn'} onClick={clearAll}>
+                            Dismiss message
+                        </button>
+                    </div>
+                </div>
+            )}
+        </form>
+    );
+};
+
+export default From;
